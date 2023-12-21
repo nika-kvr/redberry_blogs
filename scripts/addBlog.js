bd6a04c0d7472b6ac2edc5f9968594b06a5de285
Nika Kvrivishvili <nikakvrivishvili777@gmail.com>
2023-12-21 21:51:08 +0400

    write to do for check input validations

--- scripts/addBlog.js
+++ scripts/addBlog.js
@@ -93,50 +93,95 @@ function handleFileSelection(files) {
 }
 
 // Validations  
+const submitBtn = document.getElementById('submitButton');
+const authorInput = document.getElementById('authorInput');
+
+
+// get data from storage
+const form = document.getElementById('myForm');
+const inputElements = form.querySelectorAll('.input');
+inputElements.forEach(input => {
+    input.value = localStorage.getItem(input.id)
+});
+
 const validations = {
     'authorInput': [
         {
             condition: value => value.length >= 4,
-            errorMsgId: 'authorInputError1'
+            errorMsgId: 'authorInputError1',
+            isInputValid: false
         },
         {
             condition: value => value.trim().split(/\s+/).length >= 2,
-            errorMsgId: 'authorInputError2'
+            errorMsgId: 'authorInputError2',
+            isInputValid: false
         },
         {
             condition: value => /^[ა-ჰ\s]+$/u.test(value),
-            errorMsgId: 'authorInputError3'
+            errorMsgId: 'authorInputError3',
+            isInputValid: false
         }
     ],
     'headerInput': [
         {
             condition: value => value.length >= 2,
-            errorMsgId: 'headerInputError'
+            errorMsgId: 'headerInputError',
+            isInputValid: false
         }
     ],
     'aboutInput': [
         {
             condition: value => value.length >= 2,
-            errorMsgId: 'aboutInputError'
+            errorMsgId: 'aboutInputError',
+            isInputValid: false
         }
     ],
     'emailInput': [
         {
             condition: value => value.endsWith('@redberry.ge'),
-            errorMsgId: 'emailInputError'
+            errorMsgId: 'emailInputError',
+            isInputValid: false
         }
     ]
 };
 
+//check all validations for sbmt btn
+var allInputsValid = Object.keys(validations).every(inputKey =>
+    validations[inputKey].every(validation => validation.isInputValid)
+);
+
+if(allInputsValid){
+    submitBtn.disabled = false;
+}else{
+    submitBtn.disabled = true;
+}
+
+
+
 function validateInput(input) {
     const inputValue = document.getElementById(input).value;
+
+    localStorage.setItem(input, inputValue)
 
     validations[input].forEach(validation => {
         const validationSpan = document.getElementById(validation.errorMsgId);
         if(validation.condition(inputValue)){
             validationSpan.style.color = "#14D81C";
+            validation.isInputValid = true;
         }else{
             validationSpan.style.color = "#EA1919";
+            validation.isInputValid = false;
         }
+        console.log(validation.isInputValid)
     });
+
+    if(allInputsValid){
+        submitBtn.disabled = false;
+    }else{
+        submitBtn.disabled = true;
+    }
 }
+
+// add local storage if isinputvalif true or false and getItem() in allInputsValid
+
+// save date input to localstorage
