0aef914aaf102ce7d09a533cf9faf2d398502990
Nika Kvrivishvili <nikakvrivishvili777@gmail.com>
2023-12-23 17:30:04 +0400

    get categories from localstorage

--- scripts/addBlog.js
+++ scripts/addBlog.js
@@ -1,19 +1,14 @@
 // Validate logged in User
-if(!localStorage.getItem('loggedInUser')){
-    window.location.href = "index.html";
+if (!localStorage.getItem("loggedInUser")) {
+  window.location.href = "index.html";
 }
-
 
 // Select buttons
 const backBtn = document.getElementById("backBtn");
 
 backBtn.addEventListener("click", () => {
-    window.location.href = "index.html";
+  window.location.href = "index.html";
 });
-
-
-
-
 
 // Image input
 
@@ -23,263 +18,283 @@ const imgSuccessSpan = document.querySelector("#imgSuccessSpan");
 const deleteImg = document.getElementById("deleteImg");
 
 var selectedFiles = [];
-imageUrl = localStorage.getItem('imageURL');
+imageUrl = localStorage.getItem("imageURL");
 selectedFiles.push(imageUrl);
 
-
-if(imageUrl){
-    imgSuccess.style.display = "block";
-    dragArea.style.display = "none";
+if (imageUrl) {
+  imgSuccess.style.display = "block";
+  dragArea.style.display = "none";
 }
 
 deleteImg.addEventListener("click", () => {
-    localStorage.setItem('imageURL', '');
-    imgSuccess.style.display = "none";
-    dragArea.style.display = "block";
+  localStorage.setItem("imageURL", "");
+  imgSuccess.style.display = "none";
+  dragArea.style.display = "block";
 });
 
-
 dragArea.addEventListener("dragover", (event) => {
-    event.preventDefault();
-    dragArea.style.background = "#F1EFFB";
+  event.preventDefault();
+  dragArea.style.background = "#F1EFFB";
 });
 
 dragArea.addEventListener("dragleave", () => {
-    dragArea.style.background = "#F4F3FF";
+  dragArea.style.background = "#F4F3FF";
 });
 
 function allowDrop(event) {
-    event.preventDefault();
+  event.preventDefault();
 }
 
 function drop(event) {
-    event.preventDefault();
-    
-    var files = event.dataTransfer.files;
-    
-    if (files.length > 0) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            var imageUrl = e.target.result;
-            localStorage.setItem('imageURL', imageUrl);
-            
-        };
-        reader.readAsDataURL(files[0]);
-    }
-    imgSuccess.style.display = "block";
-    dragArea.style.display = "none";
+  event.preventDefault();
+
+  var files = event.dataTransfer.files;
+
+  if (files.length > 0) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      var imageUrl = e.target.result;
+      localStorage.setItem("imageURL", imageUrl);
+    };
+    reader.readAsDataURL(files[0]);
+  }
+  imgSuccess.style.display = "block";
+  dragArea.style.display = "none";
 }
 
-
-function openFileInput(){
-    var fileInput = document.getElementById('fileInput');
-    fileInput.click();
+function openFileInput() {
+  var fileInput = document.getElementById("fileInput");
+  fileInput.click();
 }
 
 function handleFileSelection(files) {
-    selectedFiles = [];
-    
-    selectedFiles.push(files[0]);
-    
-    if (selectedFiles) {
-        var reader = new FileReader();
+  selectedFiles = [];
 
-        reader.onload = function (e) {
-            localStorage.setItem('imageURL', e.target.result);
-        };
-        
-        reader.readAsDataURL(selectedFiles[0]);
-    }
-    imgSuccess.style.display = "block";
-    dragArea.style.display = "none";
+  selectedFiles.push(files[0]);
+
+  if (selectedFiles) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      localStorage.setItem("imageURL", e.target.result);
+    };
+
+    reader.readAsDataURL(selectedFiles[0]);
+  }
+  imgSuccess.style.display = "block";
+  dragArea.style.display = "none";
 }
-
-
 
 //Get categories input
-if(localStorage.getItem("categoriesArray")){
-    var selectedCategoriesarr = localStorage.getItem("categoriesArray").split(',');
-}else{
-    var selectedCategoriesarr = []
+if (localStorage.getItem("categoriesArray")) {
+  var selectedCategoriesarr = localStorage
+    .getItem("categoriesArray")
+    .split(",");
+  selectedCategoriesarr = selectedCategoriesarr.map(Number);
+} else {
+  var selectedCategoriesarr = [];
 }
 
-console.log(selectedCategoriesarr)
-
+console.log(selectedCategoriesarr);
 
 var settings = {
-    url: "https://api.blog.redberryinternship.ge/api/categories",
-    method: "GET",
-    timeout: 0,
-  };
+  url: "https://api.blog.redberryinternship.ge/api/categories",
+  method: "GET",
+  timeout: 0,
+};
 
 $.ajax(settings).done(function (response) {
-    const categories = response.data;
-    const container = $(".categories-border");
-    container.hide()
-  
-    categories.forEach(function (category) {
-      const categorie = $("<div>")
-        .text(category.title)
-        .addClass("categorie-btn")
-        .attr('id', category.id)
-        .on('click', function () {
-            const newCategorie = $("<div>")
-            .text(category.title + " x")
-            .addClass("selected-categories")
-            .attr('id', category.id)
-            .on('click', function () {
-                selectedCategoriesarr = selectedCategoriesarr.filter(function (element) {
-                    return element !== category.id;
-                });
-                localStorage.setItem("categoriesArray", selectedCategoriesarr)
-                $(this).remove();
-            })
-            .css({
-                color: category.text_color,
-                background: category.background_color,
-              });
-            if(selectedCategoriesarr.length > 0 ){
-                selectedCategoriesarr.forEach(element => {
-                    if(element == category.id){               
-                    }else{
-                        const selectedCategories = $("#selectedCategories");
-                        selectedCategories.append(newCategorie);
-                        selectedCategoriesarr.push(category.id)
-                        localStorage.setItem("categoriesArray", selectedCategoriesarr)
-                    }
-                });
+  const categories = response.data;
+  const container = $(".categories-border");
+  container.hide();
 
-            }else{
-                const selectedCategories = $("#selectedCategories");
-                selectedCategories.append(newCategorie);
-                selectedCategoriesarr.push(category.id)
-                localStorage.setItem("categoriesArray", selectedCategoriesarr)  
-            }
-
+  categories.forEach(function (category) {
+    const categorie = $("<div>")
+      .text(category.title)
+      .addClass("categorie-btn")
+      .attr("id", category.id)
+      .on("click", function () {
+        let newCategorie = $("<div>")
+          .text(category.title + " x")
+          .addClass("selected-categories")
+          .attr("id", category.id)
+          .on("click", function () {
+            selectedCategoriesarr = selectedCategoriesarr.filter(function (
+              element
+            ) {
+              return element !== category.id;
+            });
+            localStorage.setItem("categoriesArray", selectedCategoriesarr);
+            $(this).remove();
           })
-        .css({
-          color: category.text_color,
-          background: category.background_color,
-        });
-      container.append(categorie);
+          .css({
+            color: category.text_color,
+            background: category.background_color,
+          });
+        if (selectedCategoriesarr.length > 0) {
+          if (selectedCategoriesarr.includes(category.id)) {
+          } else {
+            const selectedCategories = $("#selectedCategories");
+            selectedCategories.append(newCategorie);
+            selectedCategoriesarr.push(category.id);
+            localStorage.setItem("categoriesArray", selectedCategoriesarr);
+          }
+        } else {
+          const selectedCategories = $("#selectedCategories");
+          selectedCategories.append(newCategorie);
+          selectedCategoriesarr.push(category.id);
+          localStorage.setItem("categoriesArray", selectedCategoriesarr);
+        }
+      })
+      .css({
+        color: category.text_color,
+        background: category.background_color,
+      });
+    container.append(categorie);
+  });
+  //get categories from localstorage
+
+  selectedCategoriesarr.forEach((element) => {
+    let selectedDiv = document.getElementById(element);
+    let selectedcont = document.getElementById("selectedCategories");
+    var newDiv = document.createElement("div");
+    newDiv.textContent = selectedDiv.textContent;
+    newDiv.classList.add("selected-categories");
+    var computedStyle = getComputedStyle(selectedDiv);
+    var backgroundColor = computedStyle.backgroundColor;
+    newDiv.style.backgroundColor  = backgroundColor;
+    newDiv.addEventListener("click", function () {
+      selectedCategoriesarr = selectedCategoriesarr.filter(function (element) {
+        return element !== +selectedDiv.id;
+      });
+      console.log(selectedCategoriesarr)
+      localStorage.setItem("categoriesArray", selectedCategoriesarr);
+      this.remove();
     });
+    selectedcont.appendChild(newDiv);
+    console.log(newDiv);
+  });
 });
 
 // Add listener to a categories div button
 $(document).ready(function () {
-    $('#categoriesBtnShow').click(function () {
-        $(".categories-border").toggle()
-    });
+  $("#categoriesBtnShow").click(function () {
+    $(".categories-border").toggle();
+  });
 });
 
-
-
-
-// Validations  
-const submitBtn = document.getElementById('submitButton');
-const authorInput = document.getElementById('authorInput');
-
+// Validations
+const submitBtn = document.getElementById("submitButton");
+const authorInput = document.getElementById("authorInput");
 
 // get data from storage
-const form = document.getElementById('myForm');
-const inputElements = form.querySelectorAll('.input');
-inputElements.forEach(input => {
-    input.value = localStorage.getItem(input.id)
+const form = document.getElementById("myForm");
+const inputElements = form.querySelectorAll(".input");
+inputElements.forEach((input) => {
+  input.value = localStorage.getItem(input.id);
 });
 
 const validations = {
-    'authorInput': [
-        {
-            condition: value => value.length >= 4,
-            errorMsgId: 'authorInputError1',
-            isInputValid: JSON.stringify(localStorage.getItem('authorInputError1')) || false
-        },
-        {
-            condition: value => value.trim().split(/\s+/).length >= 2,
-            errorMsgId: 'authorInputError2',
-            isInputValid: JSON.parse(localStorage.getItem('authorInputError2')) || false
-        },
-        {
-            condition: value => /^[ა-ჰ\s]+$/u.test(value),
-            errorMsgId: 'authorInputError3',
-            isInputValid: JSON.parse(localStorage.getItem('authorInputError3')) || false
-        }
-    ],
-    'headerInput': [
-        {
-            condition: value => value.length >= 2,
-            errorMsgId: 'headerInputError',
-            isInputValid: JSON.parse(localStorage.getItem('headerInputError')) || false
-        }
-    ],
-    'aboutInput': [
-        {
-            condition: value => value.length >= 2,
-            errorMsgId: 'aboutInputError',
-            isInputValid: JSON.parse(localStorage.getItem('aboutInputError')) || false
-        }
-    ],
-    'emailInput': [
-        {
-            condition: value => value.endsWith('@redberry.ge') || value.length === 0,
-            errorMsgId: 'emailInputError',
-            isInputValid: JSON.parse(localStorage.getItem('emailInputError')) || false
-        }
-    ]
+  authorInput: [
+    {
+      condition: (value) => value.length >= 4,
+      errorMsgId: "authorInputError1",
+      isInputValid:
+        JSON.stringify(localStorage.getItem("authorInputError1")) || false,
+    },
+    {
+      condition: (value) => value.trim().split(/\s+/).length >= 2,
+      errorMsgId: "authorInputError2",
+      isInputValid:
+        JSON.parse(localStorage.getItem("authorInputError2")) || false,
+    },
+    {
+      condition: (value) => /^[ა-ჰ\s]+$/u.test(value),
+      errorMsgId: "authorInputError3",
+      isInputValid:
+        JSON.parse(localStorage.getItem("authorInputError3")) || false,
+    },
+  ],
+  headerInput: [
+    {
+      condition: (value) => value.length >= 2,
+      errorMsgId: "headerInputError",
+      isInputValid:
+        JSON.parse(localStorage.getItem("headerInputError")) || false,
+    },
+  ],
+  aboutInput: [
+    {
+      condition: (value) => value.length >= 2,
+      errorMsgId: "aboutInputError",
+      isInputValid:
+        JSON.parse(localStorage.getItem("aboutInputError")) || false,
+    },
+  ],
+  emailInput: [
+    {
+      condition: (value) =>
+        value.endsWith("@redberry.ge") || value.length === 0,
+      errorMsgId: "emailInputError",
+      isInputValid:
+        JSON.parse(localStorage.getItem("emailInputError")) || false,
+    },
+  ],
 };
 
 //check all validations for sbmt btn
 
-
 function areAllInputsValid(validations) {
-    return Object.keys(validations).every(inputKey =>
-        validations[inputKey].every(validation => validation.isInputValid)
-    );
+  return Object.keys(validations).every((inputKey) =>
+    validations[inputKey].every((validation) => validation.isInputValid)
+  );
 }
 
-if(areAllInputsValid(validations)){
-    submitBtn.disabled = false;
-}else{
-    submitBtn.disabled = true;
+if (areAllInputsValid(validations)) {
+  submitBtn.disabled = false;
+} else {
+  submitBtn.disabled = true;
 }
-
-
 
 function validateInput(input) {
-    const inputValue = document.getElementById(input).value;
+  const inputValue = document.getElementById(input).value;
 
-    localStorage.setItem(input, inputValue)
+  localStorage.setItem(input, inputValue);
 
-    validations[input].forEach(validation => {
-        const validationSpan = document.getElementById(validation.errorMsgId);
-        if(validation.condition(inputValue)){
-            if(document.getElementById(input).id ==='emailInput' && inputValue.length===0){
-                validationSpan.style.display = "none";
-            }
-            validationSpan.classList.add('span-valid');
-            validationSpan.classList.remove('span-error');
-            validation.isInputValid = true;
-            localStorage.setItem(validation.errorMsgId, JSON.stringify(true))
-        }else{
-            if(document.getElementById(input).id ==='emailInput' && inputValue.length>0){
-                validationSpan.style.display = "block";
-            }
-            validationSpan.classList.add('span-error');
-            validationSpan.classList.remove('span-valid');
-            validation.isInputValid = false;
-            localStorage.setItem(validation.errorMsgId, JSON.stringify(false))
-        }
-    });
-
-
-
-    if(areAllInputsValid(validations)){
-        submitBtn.disabled = false;
-    }else{
-        submitBtn.disabled = true;
+  validations[input].forEach((validation) => {
+    const validationSpan = document.getElementById(validation.errorMsgId);
+    if (validation.condition(inputValue)) {
+      if (
+        document.getElementById(input).id === "emailInput" &&
+        inputValue.length === 0
+      ) {
+        validationSpan.style.display = "none";
+      }
+      validationSpan.classList.add("span-valid");
+      validationSpan.classList.remove("span-error");
+      validation.isInputValid = true;
+      localStorage.setItem(validation.errorMsgId, JSON.stringify(true));
+    } else {
+      if (
+        document.getElementById(input).id === "emailInput" &&
+        inputValue.length > 0
+      ) {
+        validationSpan.style.display = "block";
+      }
+      validationSpan.classList.add("span-error");
+      validationSpan.classList.remove("span-valid");
+      validation.isInputValid = false;
+      localStorage.setItem(validation.errorMsgId, JSON.stringify(false));
     }
+  });
+
+  if (areAllInputsValid(validations)) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
 }
 
 // create categories input
-// validate 
+// validate
 // validation color to localstorage
