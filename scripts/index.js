3c0e494c09f7898668e115b3a3c7c658c4b75d58
Nika Kvrivishvili <nikakvrivishvili777@gmail.com>
2023-12-26 00:59:57 +0400

    Get all blogs from server and append in html

--- scripts/index.js
+++ scripts/index.js
@@ -15,18 +15,18 @@ successbtn.addEventListener("click", () => {
   openModal.style.display = "none";
   addBlogbtn.style.display = "block";
   modal.close();
-})
+});
 
-function redirectToAddBlogs(){
+function redirectToAddBlogs() {
   window.location.href = "addBlogs.html";
 }
 
 // Check if user is logged in
-var loggedinUser = localStorage.getItem('loggedInUser');
-if(loggedinUser){
+var loggedinUser = localStorage.getItem("loggedInUser");
+if (loggedinUser) {
   openModal.style.display = "none";
   addBlogbtn.style.display = "block";
-}else{
+} else {
   openModal.style.display = "block";
   addBlogbtn.style.display = "none";
 }
@@ -43,7 +43,6 @@ $.ajax(settings).done(function (response) {
   const categories = response.data;
   const container = $("#divCategory");
 
-
   categories.forEach(function (category) {
     const paragraph = $("<button>")
       .text(category.title)
@@ -55,7 +54,6 @@ $.ajax(settings).done(function (response) {
     container.append(paragraph);
   });
 });
-
 
 //Post request email
 
@@ -73,21 +71,88 @@ $(document).ready(function () {
       contentType: "application/json",
       data: JSON.stringify(formData),
       success: function (response, status, jqXHR) {
-        if(jqXHR.status==204){
-          console.log('loged in');
+        if (jqXHR.status == 204) {
+          console.log("loged in");
           $(".login-success-div").remove();
           $(".div-dialog-success").show();
-          localStorage.setItem('loggedInUser', 'user');
-        }else{
-          console.log('invalid email')
+          localStorage.setItem("loggedInUser", "user");
+        } else {
+          console.log("invalid email");
           $("#dialogEmail").removeClass("dialog-login-btn");
           $("#dialogEmail").addClass("dialog-email-error");
-          $(".dialog-email-error-msg").show()
+          $(".dialog-email-error-msg").show();
         }
       },
       error: function (error) {
         console.log("Error:", error);
       },
     });
+  });
+});
+
+// Get all Blogs
+
+//description max size
+let paragraph = document.querySelector(".blog-description");
+let maxLength = 115;
+
+if (paragraph.textContent.length > maxLength) {
+  paragraph.textContent = paragraph.textContent.substring(0, maxLength) + "...";
+  paragraph.classList.add("truncate");
+}
+
+var settings = {
+  url: "https://api.blog.redberryinternship.ge/api/blogs",
+  method: "GET",
+  timeout: 0,
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer 68a4399dfe02608c084486976c178a211bab0ce0ff4ec2efcdbe8bca761f7a39",
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response.data);
+
+  let today = new Date().toISOString().split("T")[0];
+
+  response.data.forEach(function (blog) {
+    console.log("kaia");
+    if (blog.publish_date > today) {
+    } else {
+      let newDiv = $("<div>").attr("id", "div_" + blog.id).addClass("blog-content-div");
+
+      let image = $("<img>")
+        .attr("src", blog.image)
+        .attr("alt", "blog image")
+        .addClass("blog-img");
+      let author = $("<p>").text(blog.author).addClass("blog-author");
+      let date = $("<p>").text(blog.publish_date).addClass("blog-publish_date");
+      let title = $('<p>').text(blog.description).addClass("blog-title");
+      let categoriesDiv = $('<div>').addClass("blog-categories");
+
+      blog.categories.forEach(function(categorie) {
+        let categorieElement = $('<button>')
+          .text(categorie.title)
+          .addClass("categorie-btn-blog")
+          .css({
+            'background': categorie.background_color,
+            'color': categorie.text_color
+          });
+          categoriesDiv.append(categorieElement);
+      });
+
+      let description = $('<p>').text(blog.description).addClass("blog-description");
+      let openBlogDiv = $('<div>').addClass("open-blog-div");
+      let buttonView = $('<p>').text("სრულად ნახვა").addClass("open-blog");
+      let svg = $('<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none"><path d="M5.93415 13.0052C5.64125 13.2981 5.64125 13.773 5.93415 14.0659C6.22704 14.3587 6.70191 14.3587 6.99481 14.0659L5.93415 13.0052ZM14.2855 6.46446C14.2855 6.05024 13.9498 5.71445 13.5355 5.71446L6.78555 5.71445C6.37133 5.71445 6.03555 6.05024 6.03555 6.46445C6.03555 6.87867 6.37133 7.21445 6.78555 7.21445H12.7855V13.2145C12.7855 13.6287 13.1213 13.9645 13.5355 13.9645C13.9498 13.9645 14.2855 13.6287 14.2855 13.2145L14.2855 6.46446ZM6.99481 14.0659L14.0659 6.99478L13.0052 5.93412L5.93415 13.0052L6.99481 14.0659Z" fill="#5D37F3"/></svg>');
+
+      openBlogDiv.append(buttonView, svg)
+
+      newDiv.append(image, author, date, title, categoriesDiv, description, openBlogDiv);
+
+      $("#mainBlogsContainer").append(newDiv);
+    }
   });
 });
